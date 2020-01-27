4ce3593b4056c01d3581f7e84e1787de2a41ec61
jaapwverhees <34657795+jaapwverhees@users.noreply.github.com>
2020-01-27 16:12:40 +0100

    Delete IProductService.java

--- src/main/java/com/demo/owlElectronics/service/IProductService.java
+++
@@ -1,9 +0,0 @@
-package com.demo.owlElectronics.service;
-
-import com.demo.owlElectronics.model.Product;
-
-import java.util.Optional;
-
-public interface IProductService {
-    Optional<Product> findById(int id);
-}
